fe958699b86cb8632ed11847c27ec38eb8050d6f
Jerry-wang <wangruijie2009@gmail.com>
2011-07-14 18:33:18 +0800

    Signed-off-by: Jerry-wang <wangruijie2009@gmail.com> 接上

---
+++ src/pipe/modules/AvionicsModule.java
@@ -0,0 +1,15 @@
+/**
+ * Module interface which all modules should implement.
+ * @author Camilla Clifford
+ * @version 2003/03/22
+ */
+package pipe.modules;
+
+import pipe.common.dataLayer.DataLayer;
+
+
+public interface AvionicsModule extends Module{
+   
+   
+   
+}
